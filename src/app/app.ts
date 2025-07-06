886689bae46c73fef37ea7678f6e58afbd5bce61
Datn7 <datunashvili72@gmail.com>
2025-07-06 17:36:29 +0400

    fixed ads section display

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './features/navbar/navbar.component';
 import { FooterComponent } from './features/footer/footer.component';
-import { AdsSectionComponent } from './features/ads-section/ads-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    FooterComponent,
-    AdsSectionComponent,
-  ],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/features/hero-landing/hero-landing.component.ts
+++ src/app/features/hero-landing/hero-landing.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AdsSectionComponent } from '../ads-section/ads-section.component';
 
 @Component({
   selector: 'app-hero-landing',
-  imports: [],
+  imports: [AdsSectionComponent],
+  standalone: true,
   templateUrl: './hero-landing.component.html',
   styleUrl: './hero-landing.component.scss',
 })
