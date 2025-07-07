20eee288082463574a9f8d95b782f3edff7ffdba
Datn7 <datunashvili72@gmail.com>
2025-07-07 20:51:05 +0400

    providers page routing, content and styling

--- src/app/features/providers/providers.component.ts
+++ src/app/features/providers/providers.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  Provider,
+  ProvidersService,
+} from '../../core/services/providers.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-providers',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './providers.component.html',
-  styleUrl: './providers.component.scss'
+  styleUrl: './providers.component.scss',
 })
 export class ProvidersComponent {
+  providers: Provider[] = [];
 
+  constructor(private providersService: ProvidersService) {}
+
+  ngOnInit(): void {
+    this.providersService.getProviders().subscribe((data) => {
+      this.providers = data;
+    });
+  }
 }
